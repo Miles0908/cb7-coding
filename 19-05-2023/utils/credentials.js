0c2ae4fe7afe0c9d2e1cfb8f0717a4421ff0d90b
Miles0908 <96233085+Miles0908@users.noreply.github.com>
2023-05-19 18:07:35 +0200

    Esercitazione 19-05-2023

---
+++ 19-05-2023/utils/credentials.js
@@ -0,0 +1,19 @@
+export const usersList = [
+    {
+        id:1 ,
+        username:"pippo",
+        password:"pippoepippa"
+    },
+    {
+        id:2 ,
+        username:"topolino",
+        password:"topolinoetopolina"
+    },
+    {
+        id:3 ,
+        username:"paperino",
+        password:"paperinoepaperina"
+    },
+];
+
+
