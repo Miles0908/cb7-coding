001b2b342ab44c46ebe3157427d35f53ab69e642
Miles0908 <96233085+Miles0908@users.noreply.github.com>
2023-05-26 17:52:11 +0200

    Add files via upload

---
+++ 25-05.2023/script.js
@@ -0,0 +1,40 @@
+const qS = (type) => document.querySelector(type);
+const cE = (type) => document.createElement(type);
+
+const itemGen = (itemData) => {
+  const wrapper = cE("li");
+  wrapper.textContent = itemData.content;
+  wrapper.addEventListener("click", () => {
+    todoList = todoList.filter((todo) => todo.id !== itemData.id);
+
+    listItemRender();
+  });
+  return wrapper;
+};
+
+const onHandleSubmit = (e) => {
+  e.preventDefault();
+  todoList.push({ id: Date.now(), content: e.target[0].value });
+
+  listItemRender();
+};
+
+const listItemRender = () => {
+  textInput.value = "";
+  listEl.textContent = "";
+  todoList.forEach((todo) => {
+    listEl.appendChild(itemGen(todo));
+  });
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+};
+
+const formEl = qS("form");
+const listEl = qS(".list");
+const textInput = qS(".textInput");
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+
+listItemRender();
+
+//Event
+
+formEl.addEventListener("submit", onHandleSubmit);
